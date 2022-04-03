d764ff7ab91ff573aa97c4f85522702258ac5a65
dexffe <dexffe@gmail.com>
2022-04-03 15:06:28 +0600

    translate_base +

--- interpreter_translate.py
+++ interpreter_translate.py
@@ -5,7 +5,6 @@ def translate_text(text, target):
     return GoogleTranslator(source='auto', target=target).translate(text)
 
 
-# def translate_base(num, to_base=10, from_base=10):
 def translate_base(num, base):
     alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     b = alpha[num % base]
@@ -13,17 +12,6 @@ def translate_base(num, base):
         num = num // base
         b += alpha[num % base]
     return b[::-1]
-
-# Number = input()
-# Basein = int(input())
-# Baseout = int(input())
-#
-# # перевод из исходной в "10"
-# a = int(Number, Basein)
-# # перевод из "10" в заданную
-# a = translate_base(a, Baseout)
-#
-# print(a)
 
 
 def translate_crypto(text):
